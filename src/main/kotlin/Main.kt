96216c25708b8b1628bcde9314086089cfeee905
lskrvzhnr <41797694+lskrvzhnr@users.noreply.github.com>
2022-10-02 17:39:28 +0600

    5.2.11

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,10 @@
 fun main() {
-    val decorations = listOf("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
-
-    // eager, creates a new list
-    val eager = decorations.filter {
-        it[0] == 'p'
-    }
-    println("eager: $eager")
-
-    // lazy, will wait until asked to evaluate
-    val filtered = decorations.asSequence().filter { it[0] == 'p' }
-    println("filtered: $filtered")
-
-    // force evaluation of the lazy list
-    val newList = filtered.toList()
-    println("new list: $newList")
-
-    val lazyMap2 = decorations.asSequence().filter {it[0] == 'p'}.map {
-        println("access: $it")
-        it
-    }
+    val mysports = listOf("basketball", "fishing", "running")
+    val myplayers = listOf("LeBron James", "Ernest Hemingway", "Usain Bolt")
+    val mycities = listOf("Los Angeles", "Chicago", "Jamaica")
+    val mylist = listOf(mysports, myplayers, mycities)     // list of lists
     println("-----")
-    println("filtered: ${lazyMap2.toList()}")
+    println("Flat: ${mylist.flatten()}")
 
 
 }
